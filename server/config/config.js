dd2d43743c8b17abecc7a088f2a66b17ce8d4bcf
José Cardona <cardonarendonjd@gmail.com>
2019-05-23 11:46:13 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,9 +14,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb+srv://thunderHog:mPVaSmtbkKYEuZA9@cluster0-hy0rd.azure.mongodb.net/cafe';
+  // urlDB = 'mongodb+srv://thunderHog:mPVaSmtbkKYEuZA9@cluster0-hy0rd.azure.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
-
-urlDB = 'mongodb+srv://thunderHog:mPVaSmtbkKYEuZA9@cluster0-hy0rd.azure.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
